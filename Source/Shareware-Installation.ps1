da48c89e08dd869689c9c321e7f1e3e9f01cb70c
cliffzhu <zy.yong@gmail.com>
2022-10-12 10:47:14 -0700

    Added | Select -Last 1
    in case there are multiple Winget.exe found.
    
    $nativeAppFilePath = Get-ChildItem "C:\Program Files\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select -Last 1

--- Source/Shareware-Installation.ps1
+++ Source/Shareware-Installation.ps1
@@ -1,6 +1,5 @@
-# Install Winget software script by Cliff Y. Zhu
-# 2022-09-07
-# Example: powershell -executionpolicy bypass -file Shareware-Installation.ps1 "install" "7-zip"
+# Install Winget software by Cliff Y. Zhu
+# 
 param(
     [Parameter(Mandatory)]
     [string]$Action,
@@ -10,14 +9,14 @@ param(
      [string]$AppSource = "winget"
  )
  if (test-path -Path "c:\Windows\logs\SharewareInstall"){"The folder exists."}else {mkdir c:\Windows\logs\SharewareInstall}
- $nativeAppFilePath = Get-ChildItem "C:\Program Files\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname
+ $nativeAppFilePath = Get-ChildItem "C:\Program Files\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select-Object -Last 1
  if($nativeAppFilePath.length -lt 10){
      "Winget.exe Not Found" | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -append
      Write-Error -Message "Winget not found." -Category OperationStopped
-     exit 1}
+     exit}
 switch ($Action){
     "install"{
-    try{$response = &"$nativeAppFilePath" install $appname -s $appsource --accept-source-agreements --accept-package-agreements}
+    try{$response = &"$nativeAppFilePath" install $appname -s $appsource --accept-source-agreements --accept-package-agreements --force}
     catch{
         "Error happened. $nativeAppFilePath" | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
         Write-Error -Message "Error happened during installation." -Category OperationStopped
