f99e08a1a453991fbe49e382296bbe74a90290df
cliffzhu <zy.yong@gmail.com>
2025-08-21 10:25:22 -0700

    Update Shareware-Installation.ps1
    replace hard-coded C:

--- Source/Shareware-Installation.ps1
+++ Source/Shareware-Installation.ps1
@@ -8,27 +8,38 @@ param(
      [Parameter()]
      [string]$AppSource = "winget"
  )
- if (test-path -Path "c:\Windows\logs\SharewareInstall"){"The folder exists."}else {mkdir c:\Windows\logs\SharewareInstall}
- $nativeAppFilePath = Get-ChildItem "C:\Program Files\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select-Object -Last 1
+ if (test-path -Path "$env:SystemRoot\logs\SharewareInstall"){"The folder exists."}else {mkdir "$env:SystemRoot\logs\SharewareInstall"}
+ $nativeAppFilePath = Get-ChildItem "$env:ProgramFiles\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select-Object -Last 1
  if($nativeAppFilePath.length -lt 10){
-     "Winget.exe Not Found" | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -append
+     "Winget.exe Not Found" | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -append
      Write-Error -Message "Winget not found." -Category OperationStopped
      exit}
 switch ($Action){
     "install"{
     try{$response = &"$nativeAppFilePath" install $appname -s $appsource --accept-source-agreements --accept-package-agreements --force}
     catch{
-        "Error happened. $nativeAppFilePath" | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
-        Write-Error -Message "Error happened during installation." -Category OperationStopped
+        "Error happened during installation $appname." | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
+        Write-Error -Message "Error happened during installation $appname." -Category OperationStopped
     }}
     "uninstall"{
-    try{$response = &"$nativeAppFilePath" uninstall $appname}
+    try{$response = &"$nativeAppFilePath" uninstall $appname -s $appsource --accept-source-agreements --accept-package-agreements --force}
     catch{
-        "Error happened. $nativeAppFilePath" | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
-        Write-Error -Message "Error happened during installation." -Category OperationStopped
+        "Error happened during uninstall $appname." | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
+        Write-Error -Message "Error happened during uninstall $appname." -Category OperationStopped
+    }}
+    "upgrade"{
+    try{
+        if($appname -eq "all"){
+            $response = &"$nativeAppFilePath" upgrade --$appname --silent -s $appsource --accept-source-agreements --accept-package-agreements --force}
+        else{
+        $response = &"$nativeAppFilePath" upgrade $appname --silent -s $appsource --accept-source-agreements --accept-package-agreements --force}
+        }
+        catch{
+        "Error happened during upgrad $appname." | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -append 
+        Write-Error -Message "Error happened during upgrad $appname." -Category OperationStopped
     }}
 }
 
-if ($response -like "Successfully installed"){$response | Out-File "c:\Windows\logs\SharewareInstall\$appname.log" -force}else
-{$response | Out-File "c:\Windows\logs\SharewareInstall\ShareWareInstallation-error.log" -Append
+if ($response -like "Successfully installed"){$response | Out-File "$env:SystemRoot\logs\SharewareInstall\$appname.log" -force}else
+{$response | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -Append
 Write-Error -Message "Error happened during installation." -Category OperationStopped}
