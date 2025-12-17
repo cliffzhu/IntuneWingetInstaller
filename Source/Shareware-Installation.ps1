ef646986d4e113923971f4133819070dbb834bc1
cliffzhu <zy.yong@gmail.com>
2025-12-17 10:35:21 -0800

    Enhance winget installation script error handling
    Updated the script to check for 'winget.exe' in an additional directory and improved error handling.

--- Source/Shareware-Installation.ps1
+++ Source/Shareware-Installation.ps1
@@ -11,6 +11,9 @@ param(
  if (test-path -Path "$env:SystemRoot\logs\SharewareInstall"){"The folder exists."}else {mkdir "$env:SystemRoot\logs\SharewareInstall"}
  $nativeAppFilePath = Get-ChildItem "$env:ProgramFiles\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select-Object -Last 1
  if($nativeAppFilePath.length -lt 10){
+ $nativeAppFilePath = Get-ChildItem "${env:ProgramW6432}\WindowsApps" -Recurse -File | Where-Object name -like winget.exe | Select-Object -ExpandProperty fullname | Select-Object -Last 1
+}
+ if($nativeAppFilePath.length -lt 10){
      "Winget.exe Not Found" | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -append
      Write-Error -Message "Winget not found." -Category OperationStopped
      exit}
@@ -43,3 +46,4 @@ switch ($Action){
 if ($response -like "Successfully installed"){$response | Out-File "$env:SystemRoot\logs\SharewareInstall\$appname.log" -force}else
 {$response | Out-File "$env:SystemRoot\logs\SharewareInstall\ShareWareInstallation-error.log" -Append
 Write-Error -Message "Error happened during installation." -Category OperationStopped}
+
